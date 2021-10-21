4dda703429726914f671a0768f82902dff997397
Cherry708 <aluser8241@ieselcaminas.org>
2021-10-21 12:20:51 +0200

    Finalizado

--- src/exemples/Exemple_2_01.kt
+++ src/exemples/Exemple_2_01.kt
@@ -6,6 +6,7 @@ fun main(args: Array<String>){
     val f_in = FileInputStream("f1.txt")
     var c = f_in.read()
     //println(c::class)
+    //Para ser más precisos, c de character debería ser byte, ya que leemos bytes y c tiene un byte asignado
     while (c!=-1){
         println(c.toChar())
         c = f_in.read()
